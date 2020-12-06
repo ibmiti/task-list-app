3a67714dd3eea608a9b0f54e00da32831deccd26
Dimitri M <ibmiti@gmail.com>
2020-12-06 14:38:30 -0800

    setting up routes both get and resource - testing our index route

--- taskList/routes/web.php
+++ taskList/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TasksController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/');
+Route::get('/', [TaskController::class, 'index']);
+
+Route::resource('/', TasksController::class);
