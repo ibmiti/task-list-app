1dce7dd71d45345da2ea24070d4a8212745c27aa
Dimitri M <ibmiti@gmail.com>
2020-12-07 05:57:08 -0800

    working on create form

--- taskList/resources/views/tasks/create.blade.php
+++ taskList/resources/views/tasks/create.blade.php
@@ -13,6 +13,23 @@
             <form method="POST" action="task.store">
                 <label for="name" class="control-label">Task Name</label>
                 <input type="text" class="form-control form-control-lg" placeholder="Task Name">
+
+                <label for="description" class="control-label">Task Description</label>
+                <input type="textarea" class="form-control form-control-lg mt-3" placeholder="Task Description">
+
+
+                <label for="date" class="control-label">Due Date</label>
+                <input type="date" class="form-control form-control-lg mt-3" placeholder="">
+                <script type="text/javascript">
+                $(function() {
+                    $( "#datepicker" ).datepicker({
+                    changeMonth: true,
+                    changeYear: true
+                    });
+                });
+                </script>
+
+
             </form>
         </div>
     </div>
